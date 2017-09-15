de38f8e038f99a89ac70fb5e328ae5a56fac37db
Zia Fazal <zia.fazal@arbisoft.com>
2017-09-15 19:42:23 +0500

    check presence of required params
    updated version

--- social_engagement/handlers.py
+++ social_engagement/handlers.py
@@ -18,9 +18,12 @@ from social_engagement.tasks import task_update_user_engagement_score
 @receiver(thread_voted)
 @receiver(comment_voted)
 @receiver(thread_unfollowed)
-def thread_comment_delete_handler(sender, **kwargs):  # pylint: disable=unused-argument
+def thread_comment_signal_handler(sender, **kwargs):  # pylint: disable=unused-argument
     """
     Updates user social engagement score
     """
     post = kwargs['post']
-    task_update_user_engagement_score.delay(unicode(post.course_id), post.user_id)
+    course_id = getattr(post, 'course_id', None)
+    user_id = getattr(post, 'user_id', None)
+    if course_id and user_id:
+        task_update_user_engagement_score.delay(unicode(course_id), user_id)
