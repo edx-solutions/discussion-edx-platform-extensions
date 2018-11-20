a3bf7fdd1014606457dbb2b0fa02c4e7163d0f33
Agrendalath <piotr@surowiec.it>
2018-11-21 00:46:06 +0100

    [REVERT] Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='discussion-edx-platform-extensions',
-    version='1.2.7',
+    version='1.2.8',
     description='Social engagement management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
