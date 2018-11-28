2247ec0fcdc45866725ba3ce19b472afb4dc48e7
Piotr Surowiec <piotr@surowiec.it>
2018-11-28 04:05:05 +0100

    [YONK-1241] Optimize computing social engagement (#31)

--- social_engagement/tasks.py
+++ social_engagement/tasks.py
@@ -5,66 +5,45 @@ import logging
 import pytz
 from datetime import datetime
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models import F
 from celery.task import task
 
-from student.models import CourseEnrollment
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import CourseKey
 
-from social_engagement.engagement import update_user_engagement_score, get_social_metric_points
+from social_engagement.engagement import update_course_engagement, get_social_metric_points
 from social_engagement.models import StudentSocialEngagementScore
 
 log = logging.getLogger('edx.celery.task')
 
 
-@task(name=u'lms.djangoapps.social_engagement.tasks.task_update_user_engagement_score')
-def task_update_user_engagement_score(course_id, user_id):
-    """
-    Task to update user's engagement score
-    """
-    try:
-        update_user_engagement_score(course_id, user_id)
-    except Exception as exc:   # pylint: disable=broad-except
-        log.info(
-            "social engagement score update failure for course {} and user id {} with exception {}".format(
-                course_id,
-                user_id,
-                repr(exc)
-            )
-        )
-
-
-@task(name=u'lms.djangoapps.social_engagement.tasks.task_compute_social_scores_in_course')
+@task(
+    name=u'lms.djangoapps.social_engagement.tasks.task_compute_social_scores_in_course',
+    routing_key=settings.RECALCULATE_SOCIAL_ENGAGEMENT_ROUTING_KEY,
+)
 def task_compute_social_scores_in_course(course_id):
     """
     Task to compute social scores in course
     """
-    score_update_count = 0
     course_key = CourseKey.from_string(course_id)
     course = modulestore().get_course(course_key, depth=None)
-    user_ids = CourseEnrollment.objects.values_list('user_id', flat=True).filter(
-        is_active=1,
-        course_id=course_key
-    )
 
     if course:
-        # For each user compute and save social engagement score
-        for user_id in user_ids:
-            update_user_engagement_score(
-                course_key, user_id, compute_if_closed_course=True, course_descriptor=course
-            )
-            score_update_count += 1
+        score_update_count = update_course_engagement(
+            course_key, compute_if_closed_course=True, course_descriptor=course
+        )
+        log.info("Social scores updated for %d users in course %s", score_update_count or 0, course_id)
+
     else:
         log.info("Course with course id %s does not exist", course_id)
-    log.info("Social scores updated for %d users in course %s", score_update_count, course_id)
 
 
-@task(name=u'lms.djangoapps.social_engagement.tasks.task_handle_change_after_signal')
-def task_handle_change_after_signal(user_id, course_id, param, increment=True, items=1):
+@task(name=u'lms.djangoapps.social_engagement.tasks.task_update_user_engagement')
+def task_update_user_engagement(user_id, course_id, param, increment=True, items=1):
     """
-    Save changes and calculate score.
+    Save changes in stats and calculate score.
 
     :param param: `str` with stat that should be changed or
                   `dict[str, int]` (`stat: number_of_occurrences`) with the stats that should be changed
