b2fe31ecdb691090ae107b4c40ca9886fe0274d0
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-01-31 11:12:44 -0200

    Add parameters to compute engagement scores for inactive courses

--- social_engagement/management/commands/compute_social_engagement_score.py
+++ social_engagement/management/commands/compute_social_engagement_score.py
@@ -40,6 +40,13 @@ class Command(BaseCommand):
             metavar="True"
         ),
         make_option(
+            "-i",
+            "--inactive",
+            dest="compute_for_all_inactive_courses",
+            help="set this to True if social scores for all inactive courses need to be computed",
+            metavar="True"
+        ),
+        make_option(
             "--noinput",
             "--no-input",
             dest="interactive",
@@ -53,23 +60,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         course_id = options.get('course_id')
         compute_for_all_open_courses = options.get('compute_for_all_open_courses')
+        compute_for_all_inactive_courses = options.get('compute_for_all_inactive_courses')
         interactive = options.get('interactive')
 
         if course_id:
             task_compute_social_scores_in_course.delay(course_id)
-        elif compute_for_all_open_courses:
+        elif compute_for_all_open_courses or compute_for_all_inactive_courses:
             # prompt for user confirmation in interactive mode
             execute = query_yes_no(
-                "Are you sure to compute social engagement scores for all open courses?"
+                "Are you sure to compute social engagement scores for all selected courses?"
                 , default="no"
             ) if interactive else True
 
             if execute:
-                open_courses = CourseOverview.objects.filter(
-                    Q(end__gte=datetime.datetime.today().replace(tzinfo=UTC)) |
-                    Q(end__isnull=True)
-                )
-                for course in open_courses:
+                courses = CourseOverview.objects.none()
+                # Add active courses to queryset if compute_for_all_open_courses is True
+                if compute_for_all_open_courses:
+                    courses |= CourseOverview.objects.filter(
+                        Q(end__gte=datetime.datetime.today().replace(tzinfo=UTC)) |
+                        Q(end__isnull=True)
+                    )
+                # Add inactive courses to queryset if compute_for_all_inactive_courses is True
+                if compute_for_all_inactive_courses:
+                    courses |= CourseOverview.objects.filter(
+                        end__lt=datetime.datetime.today().replace(tzinfo=UTC)
+                    )
+
+                for course in courses:
                     course_id = unicode(course.id)
                     task_compute_social_scores_in_course.delay(course_id)
                     log.info("Task queued to compute social engagment score for course %s", course_id)
