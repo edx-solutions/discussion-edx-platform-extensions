55d50abb3a03108b45b799d3da571b996e49193c
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-02-03 08:47:35 -0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='discussion-edx-platform-extensions',
-    version='1.2.11',
+    version='1.2.12',
     description='Social engagement management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
