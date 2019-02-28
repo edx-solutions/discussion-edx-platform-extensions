77087ecb9de207c2ec332a98e521ebfc85688fee
Nasir Hussain <nasirhjafri@gmail.com>
2019-02-28 11:06:43 +0000

    MCKIN-9976 -- Added user first and last name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='discussion-edx-platform-extensions',
-    version='1.2.12',
+    version='1.2.13',
     description='Social engagement management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
